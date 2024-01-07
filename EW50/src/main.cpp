4f8832ea90f99cb66e44086c52f13b247dbd3056
phucndthomi <phucnd@thomi.com.vn>
2024-01-07 23:06:56 +0700

    [log] Tested

--- EW50/src/main.cpp
+++ EW50/src/main.cpp
@@ -1,5 +1,22 @@
 #include <Arduino.h>
 
+#include "Logger.h"
+
+Logger Log1, Log2;
+
+void my_printf(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    char buffer[100];
+    int written = vsprintf(buffer, fmt, args);
+
+    Serial.write(buffer, written);
+    Serial.printf("");
+
+    va_end(args);
+}
+
 
 void setup() {
   // Wait for Serial beginning
