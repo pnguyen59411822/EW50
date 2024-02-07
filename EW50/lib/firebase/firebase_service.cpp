1c65bffe7183c8101371e140e65fa4c15a3f9fa3
phucndthomi <phucnd@thomi.com.vn>
2024-02-08 00:00:48 +0700

    [Firebase] Define Firebase_begin

--- EW50/lib/firebase/firebase_service.cpp
+++ EW50/lib/firebase/firebase_service.cpp
@@ -6,6 +6,7 @@
 
 #include "firebase_service.h"
 #include "firebase_service.deps.h"
+#include "firebase_service.const.h"
 
 
 /* ==================================================
@@ -72,5 +73,35 @@ static FirebaseConfig config;
 ** =============================================== */
 
 
-//
+void Firebase_begin() {
+    // config.host = FIREBASE_HOST;
+
+    config.api_key = FIREBASE_API_KEY;
+    config.database_url = FIREBASE_DATABASE_URL;
+
+    // Assign the user sign in credentials
+    // auth.user.email = FIREBASE_USER_EMAIL;
+    // auth.user.password = FIREBASE_USER_PASSWORD;
+
+    // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+    Firebase.reconnectNetwork(true);
+
+    // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
+    // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+    fbdo_solar.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+    fbdo_water.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+
+    /* Assign the callback function for the long running token generation task */
+    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+    /** The id token (C++ string) will be available from config.signer.tokens.id_token
+     * if the sig-up was successful.
+     *
+     * Now you can initialize the library using the internal created credentials.
+     *
+     * If the sign-up was failed, the following function will initialize because
+     * the internal authentication credentials are not created.
+     */
+    Firebase.begin(&config, &auth);
+}
 
