d9e770bce2bc7d0ab71b60431013048683e07680
phucndthomi <phucnd@thomi.com.vn>
2024-02-19 13:47:15 +0700

    [hub-uart] -> [endpoint-uart]

--- EW50/src/main.cpp
+++ EW50/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #include "Logger.h"
+#include "uart_service.h"
 #include "hcsr04.h"
 
 
@@ -8,9 +9,11 @@ Logger Log(true);
 
 
 void setup() {
+  UART_init();
   HCSR04_setup();
 }
 
 void loop() {
   HCSR04_read();
+  UART_loop();
 }
