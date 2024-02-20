17f2694869f99bc9bcfbb8bdd1be179da52b6219
phucndthomi <phucnd@thomi.com.vn>
2024-02-20 18:23:27 +0700

    [volt] Define read_solarVolt

--- EW50/lib/voltage/voltage.cpp
+++ EW50/lib/voltage/voltage.cpp
@@ -43,7 +43,7 @@
 ** =============================================== */
 
 
-static float volt_src = 0;
+static float volt_solar = 0;
 
 
 /* ==================================================
@@ -52,7 +52,7 @@ static float volt_src = 0;
 ** =============================================== */
 
 
-//
+static void read_solarVolt();
 
 
 /* ==================================================
@@ -61,7 +61,13 @@ static float volt_src = 0;
 ** =============================================== */
 
 
-//
+void read_solarVolt(){
+
+    uint16_t adc_r2  = analogRead(VOLTAGE_SENSOR_PIN_SOLAR);
+    float    volt_r2 = (float)(adc_r2 * VOLTAGE_REF) / ADC_RESOLUTION;
+    
+    volt_solar  = volt_r2 * (RESISTOR_1 + RESISTOR_2) / RESISTOR_2; 
+}
 
 
 /* ==================================================
