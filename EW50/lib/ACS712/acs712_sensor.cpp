01e9781fbed30c8ceac060d1d694c60560ea3413
phucndthomi <phucnd@thomi.com.vn>
2024-02-20 15:31:13 +0700

    [ACS712] Define ACS712_calibrate

--- EW50/lib/ACS712/acs712_sensor.cpp
+++ EW50/lib/ACS712/acs712_sensor.cpp
@@ -71,5 +71,20 @@ static ACS712 sensor(ACS712_TYPE, ACS712_PIN);
 ** =============================================== */
 
 
-//
+// calibrate() method calibrates zero point of sensor,
+// It is not necessary, but may positively affect the accuracy
+// Ensure that no current flows through the sensor at this moment
+// If you are not sure that the current through the sensor will not leak during calibration - comment out this method
+void ACS712_calibrate(){
+
+    Log.print("\n");
+    Log.inf("[ACS712] Calibrating... ");
+    Log.inf("[ACS712] Ensure that no current flows through the sensor at this moment");
+
+    int zero = sensor.calibrate();
+
+    Log.inf("[ACS712] Done!");
+    Log.inf("Zero point for this sensor = %d", zero);
+    Log.print("\n");
+}
 
