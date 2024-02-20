68d96a8c37c078333d38e15a59d449a97262a256
phucndthomi <phucnd@thomi.com.vn>
2024-02-20 15:49:18 +0700

    [ACS712] Import to main

--- EW50/src/main.cpp
+++ EW50/src/main.cpp
@@ -3,6 +3,7 @@
 #include "Logger.h"
 #include "hcsr04.h"
 #include "voltage.h"
+#include "acs712_sensor.h"
 
 
 Logger Log(true);
@@ -15,4 +16,6 @@ void setup() {
 void loop() {
   HCSR04_read();
   Voltage_read();
+
+  Log.inf("[ACS712] current DC: %d", ACS712_getCurrentDC());
 }
