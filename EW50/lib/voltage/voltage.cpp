e93de209a0d6e63e5f0b019387e42ab82056155f
phucndthomi <phucnd@thomi.com.vn>
2024-02-20 18:21:48 +0700

    [volt] Use pin constant in pin table

--- EW50/lib/voltage/voltage.cpp
+++ EW50/lib/voltage/voltage.cpp
@@ -7,6 +7,8 @@
 #include "voltage.h"
 #include "voltage.const.h"
 
+#include "pin_table.h"
+
 
 /* ==================================================
 ** Macro definition
@@ -78,7 +80,7 @@ void Voltage_read() {
 
     intv = millis();
 
-    uint16_t adc_r2  = analogRead(VOLTAGE_PIN);
+    uint16_t adc_r2  = analogRead(VOLTAGE_SENSOR_PIN_SOLAR);
     float    volt_r2 = (float)(adc_r2 * VOLTAGE_REF) / ADC_RESOLUTION;
     
     volt_src  = volt_r2 * (RESISTOR_1 + RESISTOR_2) / RESISTOR_2; 
