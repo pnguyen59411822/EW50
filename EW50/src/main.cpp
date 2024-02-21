cf6addebf01d2a551f7db85fa01f0437d0e3875b
phucndthomi <phucnd@thomi.com.vn>
2024-02-21 15:43:05 +0700

    [flow_rate] Import to main

--- EW50/src/main.cpp
+++ EW50/src/main.cpp
@@ -4,6 +4,7 @@
 #include "hcsr04.h"
 #include "voltage.h"
 #include "acs712_sensor.h"
+#include "flow_rate.h"
 
 
 Logger Log(true);
@@ -11,11 +12,13 @@ Logger Log(true);
 
 void setup() {
   HCSR04_init();
+  FlowRate_init();
 }
 
 void loop() {
   HCSR04_read();
   Voltage_read();
+  FlowRate_read();
 
   Log.inf("[Voltage] Solar: %.2f", Voltage_getSolar());
   Log.inf("[Voltage] Water: %.2f", Voltage_getWater());
