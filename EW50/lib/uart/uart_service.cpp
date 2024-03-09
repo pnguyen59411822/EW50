a60346382f58c3fb6cb55353e1ae717696a7c90e
phucndthomi <phucnd@thomi.com.vn>
2024-03-09 10:01:12 +0700

    [uart] Remove unnecessary comment

--- EW50/lib/uart/uart_service.cpp
+++ EW50/lib/uart/uart_service.cpp
@@ -59,7 +59,7 @@ static SoftwareSerial mySerial(UART_RX_DEFAULT, UART_TX_DEFAULT);
 ** =============================================== */
 
 
-// static String getValue(String data, char separator, int index);
+//
 
 
 /* ==================================================
@@ -67,36 +67,8 @@ static SoftwareSerial mySerial(UART_RX_DEFAULT, UART_TX_DEFAULT);
 **
 ** =============================================== */
 
-/*
-String getValue(String data, char separator, unsigned int index) {
-  int found = 0;
-  int startIndex = 0;
-  int endIndex = -1;
-  int maxIndex = data.length() - 1;
-  
-  // Trả về chuỗi rỗng nếu index lớn hơn hoặc bằng số lượng phần tử
-  if (index >= maxIndex) {
-    return ""; 
-  }
 
-  for (int i = 0; i <= maxIndex && found <= index; i++) {
-    // Nếu tìm thấy ký tự phân tách thì trả về một đoạn
-    // Nếu không tìm thấy ký tự phân tách thì trả về toàn bộ
-    if (data.charAt(i) == separator || i == maxIndex) {
-      found++;
-      startIndex = endIndex + 1;
-      endIndex = (i == maxIndex) ? i + 1 : i;
-    }
-  }
-
-  if (found > index) {
-    return data.substring(startIndex, endIndex);
-  }
-
-  // Không nên đến đây (lỗi logic nếu thực hiện đúng vòng lặp)
-  return "";
-}
-/**/
+//
 
 
 /* ==================================================
